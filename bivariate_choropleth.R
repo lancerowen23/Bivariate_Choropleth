2f98056bb6bb07e98d0350f052dac768195d4b56
@psx8 <psx8@cdc.gov>
2022-06-13 18:05:05 -0400

    add script

--- bivariate_choropleth.R
+++ bivariate_choropleth.R
@@ -9,42 +9,46 @@ library(leaflet)
 library(rgdal)
 library(geojsonio)
 library(sf)
+library(ggplot2)
+library(cowplot)
 
 # Import TopoJSON and set CRS
-SVI <- topojson_read("https://raw.githubusercontent.com/lancerowen23/bivariate_choropleth/main/SVI2018_US_county.json") 
-SVI_reproj <-  st_set_crs(SVI, "EPSG:4326")
+SVI_2018 <- topojson_read("https://raw.githubusercontent.com/lancerowen23/bivariate_choropleth/main/SVI2018_US_county.json") 
+SVI_2018 <-  st_set_crs(SVI_2018, "EPSG:4326")
+SVI_2018_Albers <-SVI_2018 %>% st_transform(5070)
+
+ 
 
 # Look at extent of variables in sf dataframe
-colnames(SVI_reproj)
-plot(SVI_reproj$geometry)
-length(unique(SVI_reproj$STATE))
+colnames(SVI_2018)
+length(unique(SVI_2018$STATE))
 
 # Subset data to Lower 48
-lower48 <- SVI_reproj %>% 
+lower48 <- SVI_2018_Albers %>% 
   filter(STATE != "HAWAII" & STATE != "ALASKA")
 
 us_data <- bi_class(lower48, 
                     x = E_DISABL, 
                     y = RPL_THEMES, 
                     style = "quantile", 
-                    dim = 4)
+                    dim = 3)
 
 map <- ggplot() +
   geom_sf(data = us_data, 
           mapping = aes(fill = bi_class), 
-          color = "white", 
-          size = 0.1, 
+          color = "grey70", 
+          size = 0.1,
           show.legend = FALSE) +
-  bi_scale_fill(pal = "GrPink2", 
-                dim = 4) +
+  bi_scale_fill(pal = "DkCyan", 
+                dim = 3) +
   labs(
     title = "UNITED STATES | SVI and Disability",
     subtitle = "CDC 2018 Social Vulnerability Index"
   ) +
   bi_theme()
   
-legend <- bi_legend(pal = "GrPink2",
-                    dim = 4,
+legend <- bi_legend(pal = "DkCyan",
+                    dim = 3,
                     xlab = "Higher SVI",
                     ylab = "Higher Disability",
                     size = 10)
@@ -54,3 +58,18 @@ finalPlot <- cowplot::ggdraw() +
   draw_plot(legend, 0.1, 0.1, 0.2, 0.2)
 
 finalPlot
+
+#get correlations 
+library(corrplot)
+
+#subset selected variables
+list <- as.list(colnames(SVI_2018))
+SVI_2018_vars <- SVI_2018 %>% 
+  select(E_DISABL, E_SNGPNT, E_MINRTY, E_LIMENG, E_MUNIT) %>% 
+  st_drop_geometry()
+  
+str(SVI_2018_vars)
+pairs(SVI_2018_vars)
+
+m <- cor(SVI_2018_vars)
+corrplot(m, method = 'number', order = 'AOE')
