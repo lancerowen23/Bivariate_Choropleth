0c10cc8ef64c889d51a0d33860f23f849139430e
@psx8 <psx8@cdc.gov>
2022-09-26 23:44:51 -0400

    Adding file

--- SVI_Covid_CFR_Regression.R
+++ SVI_Covid_CFR_Regression.R
@@ -1,13 +1,13 @@
-# Multilevel Correlations between overall COVID-19 case fatality rates and SVI Rankings
+# Multilevel Regression between overall COVID-19 case fatality rates and SVI Rankings
 # Author: Lance R. Owen
 # Date: June 16, 2022
 
-pacman::p_load(geojsonio, sf, tidyverse, broom)
+pacman::p_load(geojsonio, sf, tidyverse, broom, formattable, lme4)
 
-cases <- read.csv('C:/Users/psx8/Downloads/covid_confirmed_usafacts.csv') %>% 
+cases <- read.csv('https://raw.githubusercontent.com/lancerowen23/bivariate_choropleth/main/covid_confirmed_usafacts.csv') %>% 
   select(countyFIPS, County.Name, State, X2022.06.14) %>% 
   filter(!County.Name == "Statewide Unallocated")
-deaths <- read.csv('C:/Users/psx8/Downloads/covid_deaths_usafacts.csv') %>% 
+deaths <- read.csv('https://raw.githubusercontent.com/lancerowen23/bivariate_choropleth/main/covid_deaths_usafacts.csv') %>% 
   select(countyFIPS, X2022.06.14)%>% 
   filter(!countyFIPS == 0)
 
@@ -26,44 +26,61 @@ SVI_2018 <- topojson_read("https://raw.githubusercontent.com/lancerowen23/bivari
 SVI_2018 <-  st_set_crs(SVI_2018, "EPSG:4326")
 SVI_2018_Albers <-SVI_2018 %>% st_transform(5070)
 
-# SUBSET THEMES
+# Subset themes of interest
 svi_themes <- SVI_2018 %>% 
   select(FIPS, RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4, RPL_THEMES) %>% 
   filter(!RPL_THEMES == -999)
 
-# FIX LEADING ZERO ON FIPS
+# Fix leading zero on FIPS 
 cfr$countyFIPS <- str_pad(as.character(cfr$countyFIPS), 5, pad = "0")
 
-svi_themes_cft <- merge(svi_themes, cfr_reg, by.x = "FIPS", by.y = "countyFIPS")
+# Join data
+svi_themes_cfr <- merge(svi_themes, cfr_reg, by.x = "FIPS", by.y = "countyFIPS")
 
+# Using standard lm function combined with group_by
 # Linear Regression by State
-lm_cfr_state <- svi_themes_cft %>% 
+lm_cfr_state <- svi_themes_cfr %>% 
   group_by(State) %>% 
   do(tidy(lm(cfr ~ RPL_THEMES, data = .))) %>% 
   filter(term == "RPL_THEMES") %>% 
-  arrange(desc(estimate))
+  arrange(desc(estimate)) %>% 
+  formattable(list(
+    State = formatter("span", style = ~ style(color = "grey35",font.weight = "bold")),
+    area(col = 3) ~ color_tile("#DeF7E9", "#71CA97"),
+    area(col = 4) ~ color_tile("#F7E0C9", "#F47C0A"),
+    area(col = 5) ~ color_tile("#F6F6B8","#F6EA4C"),
+    area(col = 6) ~ color_tile("#F7BAC5", "#F35D7B")))
+
+formattable(mex.cases.deaths, list(
+  Municipality = formatter("span", style = ~ style(color = "grey35",font.weight = "bold")), 
+  
+  area(col = 8) ~ color_tile("#F7BAC5", "#F35D7B"),
+  area(col = 9) ~ color_tile("#F7BAC5", "#F35D7B"),
+  area(col = 10) ~ color_tile("#C9B9D9", "#AA68B9")))
 
 # Linear Regression by Division
-lm_cfr_div <- svi_themes_cft %>% 
+lm_cfr_div <- svi_themes_cfr %>% 
   group_by(div) %>% 
   do(tidy(lm(cfr ~ RPL_THEMES, data = .))) %>% 
   filter(term == "RPL_THEMES") %>% 
   arrange(desc(estimate))
 
 # Linear Regression by Region
-lm_cfr_reg <- svi_themes_cft %>% 
+lm_cfr_reg <- svi_themes_cfr %>% 
   group_by(reg) %>% 
   do(tidy(lm(cfr ~ RPL_THEMES, data = .))) %>% 
   filter(term == "RPL_THEMES") %>% 
   arrange(desc(estimate))
 
 #Linear Regression with no Levels
-lm_cfr_overall <- lm(cfr ~ RPL_THEMES, data = svi_themes_cft)
+lm_cfr_overall <- lm(cfr ~ RPL_THEMES, data = svi_themes_cfr)
 summary(lm_cfr_overall)
 
 #Georgia
-svi_themes_cft_ga <- svi_themes_cft %>% 
+svi_themes_cfr_ga <- svi_themes_cfr %>% 
   filter(name == "Georgia")
-lm_cfr_overall_ga <- lm(cfr ~ RPL_THEMES, data = svi_themes_cft_ga)
+lm_cfr_overall_ga <- lm(cfr ~ RPL_THEMES, data = svi_themes_cfr_ga)
 summary(lm_cfr_overall_ga)
 
+#Using lme4 library
+summary(lmer(cfr ~ RPL_THEMES + (1 | State), data=svi_themes_cfr))
--- bivariate_choropleth.R
+++ bivariate_choropleth.R
@@ -9,19 +9,25 @@ pacman::p_load(biscale, tidycensus, geojsonR, leaflet, rgdal, geojsonio, sf, ggp
 # Import TopoJSON and set CRS
 SVI_2018 <- topojson_read("https://raw.githubusercontent.com/lancerowen23/bivariate_choropleth/main/SVI2018_US_county.json") 
 SVI_2018 <-  st_set_crs(SVI_2018, "EPSG:4326")
-SVI_2018_Albers <-SVI_2018 %>% st_transform(5070)
+SVI_2018_Albers <-SVI_2018 %>% 
+  st_transform(5070) %>% 
+  select(FIPS, RPL_THEMES, E_DISABL) %>% 
+  st_set_geometry(NULL)
 
+
+# Census Boundary file counties 20m
+counties_2018 <- topojson_read("https://raw.githubusercontent.com/lancerowen23/US_topojson/main/cb_2018_us_county_20m_AK_HI.json") 
+counties_2018 <-  st_set_crs(counties_2018, "EPSG:4326")
+counties_2018_Albers <-counties_2018 %>% st_transform(5070)
+
+SVI_2018_Albers <- merge(counties_2018_Albers, SVI_2018_Albers, by.x = "GEOID", by.y = "FIPS")
  
 
 # Look at extent of variables in sf dataframe
 colnames(SVI_2018)
 length(unique(SVI_2018$STATE))
 
-# Subset data to Lower 48
-lower48 <- SVI_2018_Albers %>% 
-  filter(STATE != "HAWAII" & STATE != "ALASKA")
-
-us_data <- bi_class(lower48, 
+us_data <- bi_class(SVI_2018_Albers, 
                     x = E_DISABL, 
                     y = RPL_THEMES, 
                     style = "quantile", 
@@ -49,7 +55,7 @@ legend <- bi_legend(pal = "DkCyan",
 
 finalPlot <- cowplot::ggdraw() +
   draw_plot(map, 0, 0, 1, 1) +
-  draw_plot(legend, 0.1, 0.1, 0.2, 0.2)
+  draw_plot(legend, 0.1, 0.6, 0.2, 0.2)
 
 finalPlot
 
